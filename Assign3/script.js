556a897d148cc46ff61ec6d5d78f4d8c8f042dea
Siddharth Ghawate <siddharthaghawate@gmail.com>
2026-02-03 11:48:18 +0530

    Assign3

--- Assign3/script.js
+++ Assign3/script.js
@@ -1,8 +1,10 @@
 document.getElementById("loginForm").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    let username = document.getElementById("username").value.trim();
+    let email = document.getElementById("username").value.trim();
     let password = document.getElementById("password").value.trim();
+
+    let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
     let userError = document.getElementById("userError");
     let passError = document.getElementById("passError");
@@ -14,23 +16,24 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
 
     let isValid = true;
 
-    if (username === "") {
-        userError.textContent = "Username is required";
+    if (email === "") {
+        userError.textContent = "Email is required";
+        isValid = false;
+    } 
+    else if (!emailPattern.test(email)) {
+        userError.textContent = "Enter a valid email address";
         isValid = false;
     }
 
     if (password === "") {
         passError.textContent = "Password is required";
         isValid = false;
-    } else if (password.length < 6) {
-        passError.textContent = "Password must be at least 6 characters";
+    } 
+    else if (password.length < 6) {
+        passError.textContent = "Password must be at least 6 characters long";
         isValid = false;
     }
     if (isValid) {
-        if (username === "admin" && password === "admin123") {
-            successMsg.textContent = "Login successful!";
-        } else {
-            passError.textContent = "Invalid username or password. Please enter valid username and password.";
-        }
+        successMsg.textContent = "Email and password are valid!";
     }
 });
