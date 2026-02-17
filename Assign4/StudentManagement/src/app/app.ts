433bcd248bf35d19341441071209b083e70b365d
Siddharth Ghawate <siddharthaghawate@gmail.com>
2026-02-17 11:57:59 +0530

    Angular Project

---
+++ Assign4/StudentManagement/src/app/app.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+})
+export class App {
+
+  students: any[] = [];
+
+  addStudent(form: any) {
+    this.students.push(form.value);
+    form.reset();
+  }
+}
